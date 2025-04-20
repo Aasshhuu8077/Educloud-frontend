60c55b305a4080e1b73f94253698309a80ce00bb
Ashutosh Singh <149466412+Aasshhuu8077@users.noreply.github.com>
2025-04-21 01:45:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
 
-export const server = 'http://localhost:8120'
+export const server = 'https://educloud-backend-r32q.onrender.com'
 
 
 createRoot(document.getElementById('root')).render(
